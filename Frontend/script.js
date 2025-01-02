9f8d9f812d502aa2d0b80e4448e13bf258dd2329
theMungai <mungaithesimon@gmail.com>
2025-01-02 11:58:36 +0300

    Enabled "Enter" keyboard event

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,6 +2,8 @@
 // Show Popup Dialog
 const popup = document.querySelector(".popup-container");
 const dialogs = document.querySelectorAll(".popup-dialog");
+const nextButtons = document.querySelectorAll(".next-button");
+const prevButtons = document.querySelectorAll(".prev-button");
 
 function showPopUp(){
     popup.style.display = "block";
@@ -31,15 +33,21 @@ function prevSlide(){
 // If the field is not filled, show error message
 function checkInput(){
     const userInput = document.querySelectorAll(".user-value");
-    const inputContainer = document.querySelectorAll(".user-input");
+    const inputContainer = document.querySelectorAll(".input-container");
+    const nextButtons = document.querySelectorAll(".next-button");
 
     userInput.forEach((input)=> {
-        if(input.value === ""){
-            dialogs.forEach((dialog) => {
-                const errorMessage = dialog.querySelector(".error-message");
+        input.focus()
+        if(input.value.length === 0){
+            inputContainer.forEach((container) => {
+                const errorMessage = container.querySelector(".error-message");
                 errorMessage.style.opacity = 1;
-                errorMessage.textContent = "This field is required"
-            })
+                errorMessage.textContent = "This field is required";   
+            });
+
+        }
+        else if(input.value.length > 0) {
+
         }
     })
 }
@@ -51,18 +59,27 @@ document.querySelector(".add-button-1").addEventListener("click", () => {
 });
 
 // Next Button Functionality
-const nextButtons = document.querySelectorAll(".next-button");
 nextButtons.forEach((next) => {
     next.addEventListener("click", () => {
         nextSlide();
         checkInput();
+    })
+});
+
+// When the user presses "Enter" on the keyboard
+document.querySelectorAll(".user-value").forEach((input) => {
+    input.addEventListener("keydown", (event) => {
+        if(event.key === "Enter"){
+            nextSlide();
+            checkInput();
+        }
     })
 })
 
 
 let currentSlide = 0;
 // Previous Button Functionality
-const prevButtons = document.querySelectorAll(".prev-button");
+
 prevButtons.forEach((prev) => {
     prev.addEventListener("click", () => {
         prevSlide()
